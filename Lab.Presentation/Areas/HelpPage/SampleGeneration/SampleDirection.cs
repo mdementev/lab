dc8313d504cdb981c772663e1c95ae202d348ed2
mikhail.dementev@akvelon.com <mikhail.dementev@akvelon.com>
2016-02-03 18:07:53 +0300

    Initial commit

---
+++ Lab.Presentation/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace Lab.Presentation.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
